6a738e0a5cfc7837ddc0a3d19d07f3352f422dda
vagrant <vagrant@localhost.localdomain>
2020-11-17 12:26:35 +0000

    Google_API_Language_add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_131433) do
+ActiveRecord::Schema.define(version: 2020_11_17_053627) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_11_05_131433) do
     t.datetime "updated_at", null: false
     t.text "body"
     t.string "image_id"
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "relationships", force: :cascade do |t|
